8f4add1b6b9e71ca3de21cbbb80052f8c25b00b5
CristianZelaya <criszel94@gmail.com>
2023-06-23 11:27:47 -0600

    Schema de Task
    se creo el esquema para manejar las tareas

---
+++ src/tasks/schema/task.schema.ts
@@ -0,0 +1,29 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export enum TaskStatus {
+    PENDING = 'PENDING',
+    IN_PROGRESS = 'IN_PROGRESS',
+    DONE = 'DONE'
+}
+
+export type TaskDocument = Task & Document;
+
+@Schema()
+export class Task {
+    
+  @Prop({ required: true })
+  title: string;
+
+  @Prop({ required: true })
+  description: string;
+
+  @Prop({ default: TaskStatus.PENDING})
+  statusTask: string;
+
+  @Prop({ default: true })
+  status: boolean
+
+}
+
+export const TaskSchema = SchemaFactory.createForClass(Task);
