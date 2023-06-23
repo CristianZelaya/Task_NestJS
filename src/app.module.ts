4fd026b7efb59c725266422d046a3b66d6003e27
CristianZelaya <criszel94@gmail.com>
2023-06-23 10:34:35 -0600

    Conexion a la base de datos
    Se agrego la conexion a la base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`)
+  ],
   controllers: [],
   providers: [],
 })
